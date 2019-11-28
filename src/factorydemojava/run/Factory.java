a47d9cd99676db5aec2b3eea64af4c67e7cf804e
unknown <harsha@akronym.co>
2019-11-28 17:17:46 +0800

    first commit 2

--- src/factorydemojava/run/Factory.java
+++ src/factorydemojava/run/Factory.java
@@ -7,10 +7,14 @@ public class Factory {
 
 	public static void main(String[] args) {
 		
+		//creating a machine recipe using the factory
+		//5th parameter is the userId which is not needed when creating a machine recipe
 		Recipe machineRecipe = RecipeFactory.buildRecipe("MACHINE", "Coffee machine recipe", 80, 30, null, 234523 );
 		
-		
+		//creating an user recipe using the factory
+		//6th parameter is the machineId which is not needed when creating a user recipe [I'm passing 123 to avoid syntax errors]
 		Recipe userRecipe = RecipeFactory.buildRecipe("USER", "My recipe", 80.0, 30.2, "123asd", 123 );
+		
 		
 		System.out.println(machineRecipe.toString());
 		
